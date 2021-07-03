8068c85f1ab6b562ab3860707f67fdb1e3119fce
Iraponan Marinho <iraponan.aires@outlook.com>
2021-07-03 18:30:26 -0300

    Projeto Criado

---
+++ src/main/java/br/com/iraponan/orangetalentscomicsbookschallenge/OrangeTalentsComicsBooksChallengeApplication.java
@@ -0,0 +1,13 @@
+package br.com.iraponan.orangetalentscomicsbookschallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrangeTalentsComicsBooksChallengeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrangeTalentsComicsBooksChallengeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/iraponan/orangetalentscomicsbookschallenge/OrangeTalentsComicsBooksChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.iraponan.orangetalentscomicsbookschallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrangeTalentsComicsBooksChallengeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
